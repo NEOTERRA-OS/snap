bafeeffe11ef52866f9d4233d83cf789c9c8134a
Benedikt Förtig <benediktfoertig@192.168.1.141>
2026-07-06 19:33:28 +0300

    auto: 2026-07-06 19:33:28

--- components/Icon.js
+++ components/Icon.js
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 // Minimal inline Lucide icons (MIT). No emojis per NEOS guideline.
 const P = {
   camera: '<path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"/><circle cx="12" cy="13" r="3"/>',
